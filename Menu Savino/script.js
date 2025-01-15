150d8da6eb82c7d0ad6a72578a11f2a2aaef9fab
Gabriele Savino <savinogabriele16@gmail.com>
2025-01-15 09:16:55 +0100

    Add files via upload

---
+++ Menu Savino/script.js
@@ -0,0 +1,7 @@
+document.querySelector('.btn-primary').addEventListener('click', function() {
+  alert('Ordine effettuato!');
+});
+
+document.querySelector('.btn-secondary').addEventListener('click', function() {
+  alert('Contattaci per maggiori informazioni!');
+});
