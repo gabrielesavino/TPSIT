ee1179802597756177e6e31c6c8f42dad76a88f2
Gabriele Savino <savinogabriele16@gmail.com>
2025-10-01 13:16:25 +0200

    Add files via upload

---
+++ es 2 vue.js/app.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: '#app',
+    data: {
+        isLoggedIn: false,
+        coloreSemaforo: 'rosso',
+        teamSportivi: ['Juventus', 'Barcellona', 'Arsenal'],
+        prodotti: [
+            { id: 1, nome: 'Laptop', prezzo: 899.99 },
+            { id: 2, nome: 'Mouse', prezzo: 29.99 },
+            { id: 3, nome: 'Tastiera', prezzo: 79.99 }
+        ]
+    }
+});
